fa53ecca1e06374269630ec67def9438aa239bd9
Evan Waters <wakeboarder1335@gmail.com>
2016-12-16 09:16:10 -0700

    fix file requires

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 gem "rack"
 gem "grape"
 gem "pg"
+
+gem "require_all"
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
+require 'require_all'
+require 'yaml'
 require 'grape'
 require 'pg'
 require 'virtus'
 
-Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each {|file| require file }
-
-# Require all files in app directory
-Dir["#{File.dirname(__FILE__)}/../app/**/*.rb"].each {|file| require file }
+require_rel '../lib'
+require_rel '../app'
 
 # Connect to Database
-# DB.connect(dbname: 'exercise', user: 'postgres')
+# DB.connect(dbname: 'database_name', user: 'postgres')
